637e9423d02c80e0caa822d939bd78026f2528d6
Tumelo <55387517+UnityGameDesign-max@users.noreply.github.com>
2024-04-26 21:48:07 +0200

    added Navbar on its own component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import { Flag } from "lucide-react";
+
+const NavBar = () => {
+
+    return(
+        <nav className="sticky h-14 insert-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+        <div className="max-auto w-full px-2.5 md:px-20">
+          <div className="flex h-14 items-center gap-1 border-b border-zinc-200">
+            <Flag className="h-4 w-4 text-primary"/>
+            <h1 className="text-lg font-semibold">Search</h1>
+          </div>
+        </div>
+      </nav>
+    )
+}
+
+export default NavBar;
