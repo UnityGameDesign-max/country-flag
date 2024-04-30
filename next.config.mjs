8720cd58fc59405dc6c5a6d684ab52f133c28008
Tumelo <55387517+UnityGameDesign-max@users.noreply.github.com>
2024-04-30 13:34:26 +0200

    updated configs in nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
     images: {
         domains: ['flagcdn.com', 'upload.wikimedia.org'],
     }
