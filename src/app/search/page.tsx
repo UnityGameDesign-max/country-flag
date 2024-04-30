f745fae4be84a9753de9e431f72c3608bba7dd69
Tumelo <55387517+UnityGameDesign-max@users.noreply.github.com>
2024-04-30 13:27:43 +0200

    updated suspens

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -83,89 +83,91 @@ const Page = () => {
     const currentPageCountries = countries.slice(startIndex, endIndex);
 
     return (
-        <div className="py-10 flex justify-center flex-col items-center">
-            
-            {isLoading ? <Loader className='mt-2 text-center text-primary w-5 h-5 animate-spin' />: ''}
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="py-10 flex justify-center flex-col items-center">
+                
+                {isLoading ? <Loader className='mt-2 text-center text-primary w-5 h-5 animate-spin' />: ''}
 
-            
-            <div className="mx-auto grid grid-cols-1 mt-10 sm:grid-cols-2 hl:grid-cols-1 gap-4 xl:grid-cols-2 px-5">
+                
+                <div className="mx-auto grid grid-cols-1 mt-10 sm:grid-cols-2 hl:grid-cols-1 gap-4 xl:grid-cols-2 px-5">
 
-            <Suspense fallback={<div>Loading...</div>}>
-                {currentPageCountries.length > 0 && currentPageCountries.map((country: any) => (
-                        <Card key={country.cca2} className="w-[335px] bg-slate-50 sm:w-[435px] my-3">
-                            <CardContent className="py-6 flex justify-between">
-                                <div key={country.area}>
-                                    <p className="font-semibold">{country.name.common}</p>
-                                    
-                                    {country.currencies && Object.values(country.currencies) ?  Object.values(country.currencies).map((item: any, index: number) => (
-                                        <div key={index} className="flex items-center gap-2">
-                                            <p className="text-sm">{item.name}</p>
-                                            
-                                            {item.symbol ? <div className="bg-green-500/10 p-2 rounded-lg">
-                                                <p className="text-green-500 text-sm">{item.symbol}</p>
-                                            </div> : <p className="text-sm">👎unknown</p>}
-                                            
-                                        </div>
-                                    )) : <p className="text-muted-foreground text-sm">unknown 🤷‍♀️</p>}
-                                    <p className="text-sm text-muted-foreground">{country.capital ? joinAllCapitals(country.capital) : <p>unknown ❌</p>}</p>
-                                </div>
+                
+                    {currentPageCountries.length > 0 && currentPageCountries.map((country: any) => (
+                            <Card key={country.cca2} className="w-[335px] bg-slate-50 sm:w-[435px] my-3">
+                                <CardContent className="py-6 flex justify-between">
+                                    <div key={country.area}>
+                                        <p className="font-semibold">{country.name.common}</p>
+                                        
+                                        {country.currencies && Object.values(country.currencies) ?  Object.values(country.currencies).map((item: any, index: number) => (
+                                            <div key={index} className="flex items-center gap-2">
+                                                <p className="text-sm">{item.name}</p>
+                                                
+                                                {item.symbol ? <div className="bg-green-500/10 p-2 rounded-lg">
+                                                    <p className="text-green-500 text-sm">{item.symbol}</p>
+                                                </div> : <p className="text-sm">👎unknown</p>}
+                                                
+                                            </div>
+                                        )) : <p className="text-muted-foreground text-sm">unknown 🤷‍♀️</p>}
+                                        <p className="text-sm text-muted-foreground">{country.capital ? joinAllCapitals(country.capital) : <p>unknown ❌</p>}</p>
+                                    </div>
+                                
+                                    <Image 
+                                        src={country.flags.png}
+                                        alt="flag"
+                                        width={100}
+                                        height={100}
+                                        quality={100}
+                                        objectFit="contain"
+                                    />
+                                </CardContent>
+                            </Card>
+                        ))}
+                    
+                
+                    
+                    </div>
+
+                    <div className="flex flex-col">
+                        {countries.length === 0 && !isLoading && 
+                            <div className="flex flex-col items-center">
+                                {apiFail ? 
+                                    <div className="bg-red-500/10 p-2 rounded-lg">
+                                        <Unplug className='w-20 h-20 text-red-500' />
+                                    </div>
+                                    :
+                                    <div className="bg-primary/10 p-2 rounded-lg">
+                                        <Flag className="w-20 h-20 text-primary"/>
+                                    </div>
+                                }
+                                {apiFail ? <p className="mt-4 md:text-2xl font-semibold">There is a technical issue. Please try again later.</p> :  <p className="mt-4 md:text-2xl font-semibold">No countries found for {`'${searchQuery}'`}</p>}
                             
-                                <Image 
-                                    src={country.flags.png}
-                                    alt="flag"
-                                    width={100}
-                                    height={100}
-                                    quality={100}
-                                    objectFit="contain"
+                            </div>
+                        }
+
+
+                        <Pagination>
+                            <PaginationContent>
+                                <PaginationItem>
+                                <PaginationPrevious 
+                                    className={
+                                        cn('cursor-pointer', startIndex === 0 ? 'pointer-events-none opacity-50': undefined)
+                                    }
+                                    onClick={handlePrevious}
                                 />
-                            </CardContent>
-                        </Card>
-                    ))}
-                
-            </Suspense>
-                
-                </div>
-
-                <div className="flex flex-col">
-                    {countries.length === 0 && !isLoading && 
-                        <div className="flex flex-col items-center">
-                            {apiFail ? 
-                                <div className="bg-red-500/10 p-2 rounded-lg">
-                                    <Unplug className='w-20 h-20 text-red-500' />
-                                </div>
-                                :
-                                <div className="bg-primary/10 p-2 rounded-lg">
-                                    <Flag className="w-20 h-20 text-primary"/>
-                                </div>
-                            }
-                            {apiFail ? <p className="mt-4 md:text-2xl font-semibold">There is a technical issue. Please try again later.</p> :  <p className="mt-4 md:text-2xl font-semibold">No countries found for {`'${searchQuery}'`}</p>}
-                           
-                        </div>
-                    }
-
-
-                    <Pagination>
-                        <PaginationContent>
-                            <PaginationItem>
-                            <PaginationPrevious 
-                                className={
-                                    cn('cursor-pointer', startIndex === 0 ? 'pointer-events-none opacity-50': undefined)
-                                }
-                                onClick={handlePrevious}
-                            />
-                            </PaginationItem>
-                            <PaginationItem>
-                            <PaginationNext 
-                                className={
-                                    cn('cursor-pointer', endIndex >= countries.length ? 'pointer-events-none opacity-50': undefined)
-                                }
-                                onClick={handleNext}
-                            />
-                            </PaginationItem>
-                        </PaginationContent>
-                    </Pagination>
-                </div>
-        </div>
+                                </PaginationItem>
+                                <PaginationItem>
+                                <PaginationNext 
+                                    className={
+                                        cn('cursor-pointer', endIndex >= countries.length ? 'pointer-events-none opacity-50': undefined)
+                                    }
+                                    onClick={handleNext}
+                                />
+                                </PaginationItem>
+                            </PaginationContent>
+                        </Pagination>
+                    </div>
+            </div>
+        </Suspense>
     )
 }
 
